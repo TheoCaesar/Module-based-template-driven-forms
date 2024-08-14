4f74b537c49d58c3c82dbbfca9eceeba46666d1b
TheoCaesar <theocaesar7@gmail.com>
2024-08-14 06:25:50 +0100

    setValue - not recommended

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,20 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class AppComponent {
   answer: string;
   @ViewChild('myForm') formEl;
-  default = 'pet'
-  genders = ['male', 'female']
-  
+  default = 'pet';
+  genders = ['male', 'female'];
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.formEl.setValue({
+      userdata: {
+        username: suggestedName,
+        email: 'admin@example.com'
+      },
+      secret: 'pet',
+      questionAnswer:'el perro',
+      gender:'male'
+    });
   }
 
   onSubmit(){
