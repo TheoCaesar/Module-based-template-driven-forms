f5a848d198bf243d11b0ecb5289bf02675c7009a
TheoCaesar <theocaesar7@gmail.com>
2024-08-14 06:51:48 +0100

    access form values for use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,43 @@ export class AppComponent {
   @ViewChild('myForm') formEl;
   default = 'pet';
   genders = ['male', 'female'];
+  user = {
+    username: "",
+    email: "",
+    secretQuestion:'',
+    answer: "",
+    gender: ""
+  }
+  isSubmitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    // this.formEl.setValue({
-    //   userdata: {
-    //     username: suggestedName,
-    //     email: 'admin@example.com'
-    //   },
-    //   secret: 'pet',
-    //   questionAnswer:'el perro',
-    //   gender:'male'
-    // });
-    this.formEl.form.patchValue({
+    this.formEl.setValue({
       userdata: {
-        username: suggestedName
-      }
-    })
+        username: suggestedName,
+        email: 'admin@example.com'
+      },
+      secret: 'pet',
+      questionAnswer:'el perro',
+      gender:'male'
+    });
+    // this.formEl.form.patchValue({
+    //   userdata: {
+    //     username: suggestedName
+    //   }
+    // })
   }
 
   onSubmit(){
-    console.dir(this.formEl)
+    console.dir(this.formEl);
+    this.user.username = this.formEl.value.userdata.username;
+    this.user.email = this.formEl.value.userdata.email;
+    this.user.secretQuestion = this.formEl.value.secret;
+    this.user.answer = this.formEl.value.questionAnswer;
+    this.user.gender = this.formEl.value.gender;
+    this.isSubmitted = true;
+
+    //reset form afresh
+    this.formEl.reset();
   }
 }
